d5ed2742195bc2951f39d5b6dc6ed4b58663d9db
Purnommo-DEV <purnommo404@gmail.com>
2026-01-21 00:13:51 +0700

    fixed folder manifest.json

--- vite.config.js
+++ vite.config.js
@@ -104,7 +104,7 @@ export default defineConfig({
     },
 
     build: {
-        outDir: 'public/build',
+        outDir: 'public/.vite',
         assetsDir: 'assets',
         manifest: true,
     }
