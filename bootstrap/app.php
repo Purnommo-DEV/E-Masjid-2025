e2ef769e4c26b653886bc8554823d08cdd8949be
Purnommo-DEV <purnommo404@gmail.com>
2026-01-20 17:38:05 +0700

    fixed bug v2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ use Spatie\Permission\Middleware\RoleOrPermissionMiddleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',  // ← tambahkan baris ini
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
